306e5f2fe5119e527c14532c9c4f0560dbbe3f9e
Lavanya Vijayakrishnan <lavanyavijayakrishnan@Lavanyas-MacBook-Air.local>
2025-04-20 12:33:30 -0700

    removed creds

--- beam_custom_pipeline/gcp_docker.sh
+++ beam_custom_pipeline/gcp_docker.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-export PROJECT_ID="defi-sentinal"
-export REGION="us-east4"
-export IMAGE_NAME="dataflow-python-job"
+export PROJECT_ID=""
+export REGION=""
+export IMAGE_NAME=""
 export IMAGE_TAG="V1"
 export IMAGE_URI="gcr.io/${PROJECT_ID}/${IMAGE_NAME}"
 export TEMPLATE_PATH="gs://${PROJECT_ID}-dataflow-bucket/templates/dataflow_job_template.json"
